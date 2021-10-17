d91cbc047bc826f692e41670b9e02340f453ea28
SeongMin-K <eo1tks3@naver.com>
2021-10-17 19:56:55 +0900

    Initial Commit

---
+++ Tooltip_Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tooltip_Tutorial
+//
+//  Created by SeongMinK on 2021/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
